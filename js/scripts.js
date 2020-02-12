2bbb6acb78b3e66ae2ce8dd39a1b808d2b7973ed
Vincent Dang <vncntdang@gmail.com>
2020-02-11 21:47:31 -0500

    Implemented user button clicks and display markers on the grid for easy mode

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 // Scripts starts here
 const myTicTacToeApp = {};
 
-myTicTacToeApp.welcomeModalOptions = function() {
-    
+// Handle welcome form inputs when user submits
+myTicTacToeApp.handleWelcomeModalOptions = function() {
     const welcomeModal = document.querySelector('#welcomeModal');
-    const submitForm = document.querySelector('#welcome__optionsForm');
+    const submitForm = document.querySelector('#welcomeOptionsForm');
 
     submitForm.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -12,13 +12,66 @@ myTicTacToeApp.welcomeModalOptions = function() {
         const selectedMode = e.currentTarget.mode.value;
         const selectedMarker = e.currentTarget.marker.value;
         
+        this.numOfSpotsTaken = 0;
         this.userMarker = selectedMarker;
         this.currentMode = selectedMode;
         this.closeModal(welcomeModal);
     })
 }
 
-myTicTacToeApp.getComputerMarker = () => {
+myTicTacToeApp.disableButton = function(button) {
+    button.classList.add('disabled');
+    button.disabled = true;
+}
+
+// Handle user button clicks function
+myTicTacToeApp.handleUserButtonClick = function() {
+    myTicTacToeApp.numOfSpotsTaken += 1;
+    myTicTacToeApp.disableButton(this);
+    this.textContent = myTicTacToeApp.userMarker;
+    myTicTacToeApp.handleComputerEvents();
+}
+
+// Handle grid buttons by setting click events for each of them
+myTicTacToeApp.handleGridButtons = function() {
+
+    const gameGridButtons = document.querySelectorAll('.gameGrid__button');
+
+    gameGridButtons.forEach(button => {
+        button.addEventListener('click', this.handleUserButtonClick);
+    });
+}
+
+myTicTacToeApp.handleComputerEvents = function() {
+    if (this.currentMode === "easy") {
+        this.easyMode();
+    }
+    else {
+        this.hardMode();
+    }
+}
+
+myTicTacToeApp.easyMode = function() {
+    const computerMarker = this.getComputerMarker();
+    const randomNum = this.getRandomNumber();
+    const gridCell = document.getElementById(`cell${randomNum}`);
+
+    if (!gridCell.textContent) {
+        this.numOfSpotsTaken += 1;
+        this.disableButton(gridCell);
+        gridCell.textContent = computerMarker;
+    }
+    else if (gridCell.textContent && this.numOfSpotsTaken < 8){
+        this.handleComputerEvents();
+    }
+}
+
+myTicTacToeApp.hardMode = function() {
+
+}
+
+// Get computer marker based on user selected marker
+myTicTacToeApp.getComputerMarker = function() {
     let computerMarker = '';
 
     if (this.userMarker === 'x') {
@@ -43,7 +96,7 @@ myTicTacToeApp.closeModal = function(modal) {
 
 // Generate a random number function
 myTicTacToeApp.getRandomNumber = function() {
-    return Math.floor(Math.random() * 9);
+    return Math.floor(Math.random() * 9) + 1;
 }
 
 // Gets the current date year and displays it
@@ -54,7 +107,8 @@ myTicTacToeApp.displayCurrentYear = function() {
 
 // Run current year function
 myTicTacToeApp.init = function() {
-    this.welcomeModalOptions();
+    this.handleWelcomeModalOptions();
+    this.handleGridButtons();
     this.displayCurrentYear();
 };
 
