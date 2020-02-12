328dfbb65a6c09e1f66b05165c25cdbc33b04028
Vincent Dang <vncntdang@gmail.com>
2020-02-11 20:04:40 -0500

    Implemented user input submission on welcome modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,65 @@
 // Scripts starts here
 const myTicTacToeApp = {};
 
-myTicTacToeApp.displayWelcomeModal = function() {
+myTicTacToeApp.welcomeModalOptions = function() {
     
+    const welcomeModal = document.querySelector('#welcomeModal');
+    const submitForm = document.querySelector('#welcome__optionsForm');
+
+    submitForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const selectedMode = e.currentTarget.mode.value;
+        const selectedMarker = e.currentTarget.marker.value;
+        
+        this.userMarker = selectedMarker;
+        this.currentMode = selectedMode;
+        this.closeModal(welcomeModal);
+    })
 }
 
-// Gets the date year and displays it
+myTicTacToeApp.getComputerMarker = () => {
+    let computerMarker = '';
+
+    if (this.userMarker === 'x') {
+        computerMarker = 'o';
+    }
+    else {
+        computerMarker = 'x';
+    }
+
+    return computerMarker;
+}
+
+// Open a modal function
+myTicTacToeApp.openModal = function(modal) {
+    modal.style.display = "block";
+}
+
+// Close a modal function
+myTicTacToeApp.closeModal = function(modal) {
+    modal.style.display = "none";
+}
+
+// Generate a random number function
+myTicTacToeApp.getRandomNumber = function() {
+    return Math.floor(Math.random() * 9);
+}
+
+// Gets the current date year and displays it
 myTicTacToeApp.displayCurrentYear = function() {
-    document.getElementById('year').innerHTML = new Date().getFullYear();    
+    const footerYear = document.getElementById('year');
+    footerYear.textContent = new Date().getFullYear();    
 }
 
+// Run current year function
 myTicTacToeApp.init = function() {
-    myTicTacToeApp.displayWelcomeModal();
-    myTicTacToeApp.displayCurrentYear();
+    this.welcomeModalOptions();
+    this.displayCurrentYear();
 };
 
-document.addEventListener('DOMContentLoaded', function(event) {
+// When DOM is finished loading, run init
+document.addEventListener('DOMContentLoaded', function() {
     myTicTacToeApp.init();
 });
 
